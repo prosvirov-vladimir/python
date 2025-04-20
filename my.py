c1ba12239cc9ac389a53301183d12e56ffe46e70
prosvirov-vladimir <xprosvirovx@gmail.com>
2025-04-20 11:49:18 +0200

    Update my.py

--- my.py
+++ my.py
@@ -3,3 +3,36 @@ print("Vladimir")
 print("Вторая строка")
 
 print("Третья строка")
+
+print("Привет", "мир", "!")
+
+print(55, 67, 78.5, True, False)
+
+a = 5
+b = 10
+print(a + b)
+print("5 + 10 =", a + b)
+print("5 + 10 =", 5 + 10)
+
+print("4", "5", "6", "7", sep="-")
+
+a = 17
+print(a)
+a = b = f = 7
+print(a, b, f)
+
+a -= 1
+a = a - 1
+print(a)
+
+print(5/2)
+print(5//2)
+
+print(5 * 2)
+print(5**2)
+
+a = 4
+b = 10
+print("a =", a, "b =", b)
+a, b = b, a
+print("a =", a, "b =", b)
