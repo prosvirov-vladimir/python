10fc030c6c52700da9e1b9b72d57df8a81d77b61
prosvirov-vladimir <xprosvirovx@gmail.com>
2025-05-18 11:56:18 +0200

    Update files

--- while-for.py
+++ while-for.py
@@ -13,27 +13,38 @@
 # менеджеров. Определить их зарплату, определить лучшего менеджера, начислить ему премию
 # 200$, вывести итоги на экран.
 
-sales1 = float(input("Введите уровень продаж первого менеджера: "))
-sales2 = float(input("Введите уровень продаж второго менеджера: "))
-sales3 = float(input("Введите уровень продаж третьего менеджера: "))
+# sales1 = float(input("Введите уровень продаж первого менеджера: "))
+# sales2 = float(input("Введите уровень продаж второго менеджера: "))
+# sales3 = float(input("Введите уровень продаж третьего менеджера: "))
+#
+# salaries = []
+# for sales in [sales1, sales2, sales3]:
+#     if sales <= 500:
+#         percent = 0.03
+#     elif sales <= 1000:
+#         percent = 0.05
+#     else:
+#         percent = 0.08
+#     salary = 200 + sales * percent
+#     salaries.append(salary)
+#
+# # Определяем лучшего менеджера
+# best_salary = max(salaries)
+# best_index = salaries.index(best_salary)
+# salaries[best_index] += 200
+#
+# print(f"Зарплата 1 менеджера: {salaries[0]}")
+# print(f"Зарплата 2 менеджера: {salaries[1]}")
+# print(f"Зарплата 3 менеджера: {salaries[2]}")
+# print(f"Лучший менеджер: {best_index + 1}-ый")
 
-salaries = []
-for sales in [sales1, sales2, sales3]:
-    if sales <= 500:
-        percent = 0.03
-    elif sales <= 1000:
-        percent = 0.05
-    else:
-        percent = 0.08
-    salary = 200 + sales * percent
-    salaries.append(salary)
+# for x in range(5, 50, 5):
+#     print(x)
+#
+# print("-------------")
+#
+# for x in range(50, 5, -5):
+#     print(x)
 
-# Определяем лучшего менеджера
-best_salary = max(salaries)
-best_index = salaries.index(best_salary)
-salaries[best_index] += 200
-
-print(f"Зарплата 1 менеджера: {salaries[0]}")
-print(f"Зарплата 2 менеджера: {salaries[1]}")
-print(f"Зарплата 3 менеджера: {salaries[2]}")
-print(f"Лучший менеджер: {best_index + 1}-ый")
+# for x, y in enumerate(["Первый", "Второй", "Третий"]):
+#     print(x, " - ", y)
