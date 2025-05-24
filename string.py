7ac79cc641d50bb3089e0451629643088d499ad5
prosvirov-vladimir <xprosvirovx@gmail.com>
2025-05-24 12:03:37 +0200

    Update files

---
+++ string.py
@@ -0,0 +1,23 @@
+# a = [1,2,3,11,12,32,11,-5]
+# print(min(a))
+# print(max(a))
+# print(len(a))
+# print(a[len(a)-1])
+# print(a.index(12))
+# print(a[3])
+# print(a.count(11))
+
+# string = "Привет мир!"
+# print(string.count("!"))
+
+string = "привет мир, Как у Тебя дела? ещё одна фраза. а затем вторая"
+print(string.capitalize())
+print(string.title())
+print(string.upper())
+print(string.lower())
+print(string.swapcase())
+
+low = string.lower()
+print(low.isupper())
+print(low.islower())
+print(low.istitle())
