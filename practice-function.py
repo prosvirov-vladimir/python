1970d849f4493efe0823ff95a6bd934c1c605362
prosvirov-vladimir <xprosvirovx@gmail.com>
2025-06-14 11:41:07 +0200

    Update files

--- practice-function.py
+++ practice-function.py
@@ -50,3 +50,66 @@
 #
 # print_triangle()
 
+# задача 7
+# Найти периметр треугольника, заданного координатами своих вершин.
+# (Определить функцию для расчета длины отрезка по координатам его  вершин.)
+# def distance(x1, y1, x2, y2):
+#     return (((x2 - x1)**2) + ((y2 - y1)**2))**0.5
+#
+# def triangle_perimetr(x1, y1, x2, y2, x3, y3):
+#     a = distance(x1, y1, x2, y2)
+#     b = distance(x2, y2, x3, y3)
+#     c = distance(x3, y3, x1, y1)
+#     return a + b + c
+#
+# print(triangle_perimetr(1, 2, 4, 5, 6, 7))
+
+
+# задача 8
+# Найти все трехзначные простые числа.
+# (Определить функцию, позволяющую  распознавать простые числа.)
+
+# def is_prime(n):
+#     if n < 2: return False
+#     for i in range(2, int(n**0.5)+1):
+#         if n % i == 0:
+#             return False
+#     return True
+
+# primes = [i for i in range(100, 1000) if is_prime(i)]
+# primes = []
+# for i in range(100, 1000):
+#     if is_prime(i):
+#         primes.append(i)
+# print(primes)
+
+# Задача 9
+# Получить все шестизначные счастливые номера.
+# Счастливым называют такое  шестизначное число, в котором сумма
+# его первых трёх цифр равна сумме его  последних трёх цифр.
+# (Определить функцию для расчета суммы цифр  трёхзначного числа.)
+
+# def sum_digits(n):
+#     summa = 0
+#     for d in str(n):
+#         summa += int(d)
+#     return summa
+#
+# def is_lucky(n):
+#     s = str(n)
+#     if sum_digits(s[:3]) == sum_digits(s[3:]):
+#         print("Число счастливое")
+#     else:
+#         print("Число не счастливое")
+#
+# is_lucky(123456)
+
+# Задача 10
+# Даны шесть различных чисел.
+# Определить максимальное из них.
+# (Определить функцию, находящую максимум из двух различных чисел.)
+
+# def max6(a, b, c, d, e, f):
+#     return max(a, b, c, d, e, f)
+#
+# print(max6(2,4,5,6,89, 9))
